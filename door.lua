eba497d730a1cda3ba6e2f82356354be4143fe11
Syntax Error <63340001+Alpha-404@users.noreply.github.com>
2021-08-22 09:08:15 +0100

    Create door.lua

---
+++ door.lua
@@ -0,0 +1,14 @@
+local open = false
+
+script.Parent.Touched:Connect(function(hit)
+	if hit.Parent:FindFirstChild("Humanoid") then
+		if open == true then return end
+		open = true
+      script.Parent.CanCollide = false
+      script.Parent.Transparency = 0.5
+      wait(3)
+      script.Parent.CanCollide = true
+      script.Parent.Transparency = 1
+    open = false
+	end
+end)
